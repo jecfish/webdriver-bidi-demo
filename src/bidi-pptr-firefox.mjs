eb56be6e73d63ba88b08b337961307c2b9a70165
Jecelyn Yeen <jec@google.com>
2023-12-13 09:54:11 +0800

    remove console

--- src/bidi-pptr-firefox.mjs
+++ src/bidi-pptr-firefox.mjs
@@ -1,8 +1,7 @@
 import * as assert from "node:assert";
 import puppeteer from "puppeteer";
 
-// Launch WebDriver BiDi
-// Install firefox: npx puppeteer browsers install firefox
+// Launch WebDriver BiDi, Install firefox manually: npx puppeteer browsers install firefox
 const browser = await puppeteer.launch({
   product: 'firefox',
   protocol: 'webDriverBiDi',
@@ -23,7 +22,6 @@ await page.setViewport({width: 600, height: 1041});
 await page.goto('https://coffee-cart.app/?breakable=1', { waitUntil: 'networkidle0' });
 
 const coffee = await page.$('[data-test="Espresso"]');
-console.log(coffee);
 await coffee.click();
 
 browser.close();
