347e73b0b2aac9c640ce5372344ba52f705b7069
Jecelyn Yeen <jec@google.com>
2023-12-15 00:19:24 +0800

    Update example

--- src/bidi-pptr-firefox.mjs
+++ src/bidi-pptr-firefox.mjs
@@ -1,31 +1,32 @@
-import * as assert from "node:assert";
-import puppeteer from "puppeteer";
+import * as assert from 'node:assert'; 
+import puppeteer from 'puppeteer';
 
-// Launch WebDriver BiDi, Install firefox manually: npx puppeteer browsers install firefox
+// Arrange: Launch browser with WebDriver BiDi
 const browser = await puppeteer.launch({
-  product: 'firefox',
   protocol: 'webDriverBiDi',
-  headless: false
+     product: 'firefox', // or 'chrome'
 });
-
 const context = await browser.createIncognitoBrowserContext();
 const page = await context.newPage();
 
-// Monitor console messages
+// Arrange: Monitor console messages
 page.on('console', message => {
   if (message.type() != 'error') return;
   console.log('RECEIVED: %s', message.text());
   // assert.fail(`Unexpected console message received: ${message.text()}`);
 });
 
+// Action
 await page.setViewport({width: 600, height: 1041});
-await page.goto('https://coffee-cart.app/?breakable=1', { waitUntil: 'networkidle0' });
+await page.goto('https://coffee-cart.app/?breakable=1');
 
-const coffee = await page.$('[data-test="Espresso"]');
+const coffee = await page.waitForSelector('[data-test="Espresso"]');
 await coffee.click();
 
+// Assert 
 const checkout = await page.$('[data-test="checkout"]');
 const total = await checkout.evaluate(el => el.textContent);
 assert.equal(total, 'Total: $10.00');
 
 browser.close();
+
